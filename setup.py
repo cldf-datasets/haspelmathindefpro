4f14fa380d3cac336a58fcfe1af23277da0090d6
Johannes Englisch <johannes_englisch@eva.mpg.de>
2025-04-25 10:25:47 +0200

    vcs

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_haspelmathindefpro',
+    py_modules=['cldfbench_haspelmathindefpro'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'haspelmathindefpro=cldfbench_haspelmathindefpro:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench[glottolog,excel]',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
